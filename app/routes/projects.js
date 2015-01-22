ea51885d8352ee926ec97c1a2f84663f91a71157
lkbhas <lbhasemeyer@gmail.com>
2015-01-21 22:11:35 -0700

    finished 3 pages css, looping

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {id: 1, title: 'Mucklucks', body: "is a haven for travelers and wanderlusters.  It makes travel research easy, visual, collaborative, and fun.  It is also a place to gather personal travel memories.", url: "https://mucklucks.herokuapp.com/"},
-    {id: 2, title: 'gCamp', body: "is a Rails app that has some of the same functionality as Basecamp. Users can create projects, tasks, comments, memberships, and also link their Pivotal Tracker projects.", url: "https://secure-dawn-1128.herokuapp.com/"},
+      {id: 2, title: 'gCamp', body: "is a Rails app that has some of the same functionality as Basecamp. Users can create projects, tasks, comments, memberships, and also link their Pivotal Tracker projects.", url: "https://secure-dawn-1128.herokuapp.com/"},
     ];
   }
 });
--- app/routes/resume.js
+++ app/routes/resume.js
@@ -5,19 +5,19 @@ export default Ember.Route.extend({
     return [
   {id: 1,
    title: 'Traveler and Blogger',
-   at: 'World Trip',
-   body: "•Fostered cross-cultural relationships and communicated with diverse groups of people"},
+   employer: 'World Trip',
+   body: ["Fostered cross-cultural relationships and communicated with diverse groups of people"]},
   {id: 2,
    title: 'Account Manager, Experience Consultant',
-   at: 'Cloud 9 Living, Boulder, CO',
-   body: "•Solidified and grew partnerships with JPMorgan Chase and Intercontinental Hotels Group.",
-   body2: "•Created experience listings, merchandised, and used basic SEO for www.cloud9living.com.",
-   body3: "•First point of contact for suppliers - fostered positive supplier relationships. "},
+   employer: 'Cloud 9 Living, Boulder, CO',
+   body: ["Solidified and grew partnerships with JPMorgan Chase and Intercontinental Hotels Group.",
+   "Created experience listings, merchandised, and used basic SEO for www.cloud9living.com.",
+   "First point of contact for suppliers - fostered positive supplier relationships."]},
   {id: 3,
    title: 'Intern',
-   at: 'Habitat for Humanity, Santa Barbara, CA',
-   body: "•Managed and maintained online sales websites for ReStore merchandise.",
-   body2: "•Planned and managed events, recruited volunteers, developed publicity"}
+   employer: 'Habitat for Humanity, Santa Barbara, CA',
+   body: ["Managed and maintained online sales websites for ReStore merchandise.",
+   "Planned and managed events, recruited volunteers, developed publicity"]}
     ];
   }
 });
